08d69d36d0d4210b3a88c550dc1d4169539cfbb4
Николай Сухинин <nikolaisukhinin@MacBook-Pro-Nikolaj.local>
2025-05-29 14:06:41 +0300

    змінили код

--- task2.py
+++ task2.py
@@ -1,25 +1,10 @@
-def select(grades: list, passgrade =60) -> list: 
+a,b = map(int, input().split())
+s= a+b
+d = a*b
+r = a-b 
+t = a/b
+print(s,d,r,t)
 
-    """
-   Функція приймає на вчід список з дійних чисел та повертає від фільтрований список із значень за введене число
-    """
-    result =[]
-    for ele in grades:
-        if ele >= passgrade:
-            result.append(ele)
-    return result 
-
-
-
-greatbook = {"Kolya": [90, 54,88, 3, 100],
-              "Sonya": [54, 43, 88, 90, 6],
-                "Ruslan":[12, 99, 88, 45]}
-
-for key in greatbook:
-   a=select( greatbook[key])
-   print (f"{key}:", *a,  f"Average : {sum(a)/len(a)}")
-
-    
 
 
 
